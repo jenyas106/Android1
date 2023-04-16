9816f84363df397156dd9d87e1f5735dccde0206
jenyas106 <jenyas106@gmail.com>
2023-04-17 02:45:18 +0300

    Dice roll logic

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button3)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+
+    }
+
+    private fun rollDice() {
+        val dice = (Dice(6))
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView2)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int){
+    fun roll():Int{
+        return (1..numSides).random()
     }
 }
